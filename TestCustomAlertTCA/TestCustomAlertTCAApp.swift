956f41f20c8e1a64e636d52524d3958cea93ad51
Roberto Pastor <roberto.pastor@cabify.com>
2024-06-28 08:24:11 +0200

    Initial Commit

---
+++ TestCustomAlertTCA/TestCustomAlertTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCustomAlertTCAApp.swift
+//  TestCustomAlertTCA
+//
+//  Created by Roberto Pastor on 28/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TestCustomAlertTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
